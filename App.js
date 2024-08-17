966b0347b2be6f112f48751672a6cf897d07e02b
Aayush Ghimire <aausgh@gmail.com>
2024-08-17 20:52:28 +0545

    feat: added image picker

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
+
+const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowEditing: true,
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      console.log(result)
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('You did not select any image')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer PlaceholderImage={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button label="Choose a photo" theme='primary' onPress={pickImage} />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +43,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  buttonContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
---
+++ components/ImageViewer.js
@@ -0,0 +1,19 @@
+import { Image, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function ImageViewer({ PlaceholderImage, selectedImage }) {
+
+   const imageSource = selectedImage ? { uri: selectedImage } : PlaceholderImage
+
+   return (
+      <Image source={imageSource} style={styles.image} />
+   )
+}
+
+const styles = StyleSheet.create({
+   image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+   },
+})
